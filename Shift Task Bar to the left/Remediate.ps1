31e35efa54f80ac61744c01a7cdb06347ac89d71
Mr B <iTranc3@gmail.com>
2025-07-01 07:44:19 +1000

    Add files via upload

---
+++ Shift Task Bar to the left/Remediate.ps1
@@ -0,0 +1,30 @@
+function Test-RegistryValue {
+    param (
+     [parameter(Mandatory=$true)]
+     [ValidateNotNullOrEmpty()]$Path,
+    
+    [parameter(Mandatory=$true)]
+     [ValidateNotNullOrEmpty()]$Value
+    )
+    
+    try {
+        Get-ItemProperty -Path $Path | Select-Object -ExpandProperty $Value -ErrorAction Stop | Out-Null
+        return $true
+    }catch {
+        return $false
+    }
+}
+
+$path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+$value = "TaskbarAl"
+
+if(Test-Path $path){
+    try{
+        Set-ItemProperty -Path $path -Name $value -Value 0 -Force
+        Exit 0
+    }catch{
+        Exit 1
+    }
+}else{
+    Exit 1
+}
